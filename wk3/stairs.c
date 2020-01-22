093cd28988d60e8bf4c48c24d6dbc15fceecd01c
Ece 15 wi20 Test account <ee15wi20zy@ieng6-247.ucsd.edu>
2020-01-22 01:13:44 -0800

    wk3

---
+++ wk3/stairs.c
@@ -0,0 +1,13 @@
+#include <karel.h>
+
+// This is where the C-program starts
+int main() {
+
+	// Start the karel program and load the settings
+	karel_setup("settings/settings01_stairs.json");
+    
+    // What follows next is the actual code controlling Karel
+
+    turn_off();				// Done
+
+}
---
+++ wk3/street.c
@@ -0,0 +1,14 @@
+#include <karel.h>
+
+
+// This is where the C-program starts
+int main() {
+
+	// Start the karel program and load the settings
+	karel_setup("settings/settings01_street.json");
+    
+    // What follows next is the actual code controlling Karel
+
+	turn_off();				// Done
+
+}
