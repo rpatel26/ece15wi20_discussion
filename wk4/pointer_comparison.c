2a8642d07228b289bc309a4ff71a8ae718e692fb
Ece 15 wi20 Test account <ee15wi20zy@ieng6-247.ucsd.edu>
2020-01-28 17:27:16 -0800

    wk4

---
+++ wk4/pointer_comparison.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// create the function here
+int main(){
+    // call the function here
+    // don't forget to initialize variables accordingly
+    return 0;
+}
--- wk4/pointer_review.c
+++ wk4/pointer_review.c
@@ -11,8 +11,8 @@ int main(){
 
     printf("value of a = %d\n", a);         // print 1
     printf("value of b = %d\n", b);         // print 2
-    printf("value of &a = %d\n", &a);       // print 3
-    printf("value of ptr = %d\n", ptr);     // print 4
+    printf("value of &a = %lu\n", &a);       // print 3
+    printf("value of ptr = %lu\n", ptr);     // print 4
     printf("value of *ptr = %d\n", *ptr);   // print 5
     return 0;
 }
