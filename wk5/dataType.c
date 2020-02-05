92ac97bc585b0416e95032592fc7fd1634e4f6e0
Ece 15 wi20 Test account <ee15wi20zy@ieng6-247.ucsd.edu>
2020-02-05 14:18:39 -0800

    changed function name

--- wk5/dataType.c
+++ wk5/dataType.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-void problem1(){
+void dataType1(){
     char c = 127;
     c = c + 1;
     printf("%d\n", c);
 }
 
-void problem2(){
+void dataType2(){
     float c = 5.0;
     printf("Temperature in Fahrenheit is %.2f\n", (9/5)*c + 32);
 }
 
-void problem3(){
+void dataType3(){
     float c = 5.0;
     printf("Temperature in Fahrenheit is %.2f\n", (9.0/5)*c + 32);
 }
 
 int main(){
-    problem1();
-    problem2();
-    //problem3();
+    dataType1();
+    // dataType2();
+    // dataType3();
     return 0;
 }
